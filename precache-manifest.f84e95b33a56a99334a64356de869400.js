8711ca777393b22196eaf053df93c68f97335163
Vyacheslav Tilikov <devil.ster.1@gmail.com>
2020-09-24 14:09:43 +0300

    Updates

---
+++ precache-manifest.f84e95b33a56a99334a64356de869400.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f043554cb176673611b1ca8057f77d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b6e7694535b01ef496f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "55f1928d1eb73cd42ad6",
+    "url": "/static/js/2.72c5812b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72c5812b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6e7694535b01ef496f",
+    "url": "/static/js/main.e1719472.chunk.js"
+  },
+  {
+    "revision": "9d19b7b61909d4c253c3",
+    "url": "/static/js/runtime-main.4315c75d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
